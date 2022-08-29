f6cb59d4e22ba4d5315008e49ea033344a4ff77a
c-future <cfuture@proton.me>
2022-08-30 00:52:24 +0300

    add gagues automatic

--- migrations/24_deploy_idle_pools.js
+++ migrations/24_deploy_idle_pools.js
@@ -1,8 +1,13 @@
 const { getContract } = require("./helper/addContracts");
 const { logTransaction } = require("./helper/logger.js");
-
+const Booster = artifacts.require("Booster");
 const PoolManager = artifacts.require("PoolManager");
 const IERC20 = artifacts.require("IERC20");
+const BigNumber = require("bignumber.js");
+const gaugeControllerABI = require("./helper/gaugeControllerABI.json");
+function toBN(number) {
+  return new BigNumber(number);
+}
 
 async function fundLpToken(lp_tokens, lp_tokens_users, to) {
   for (var i = 0; i < lp_tokens_users.length; i++) {
@@ -18,10 +23,12 @@ async function fundLpToken(lp_tokens, lp_tokens_users, to) {
 
 module.exports = async function (deployer, network, accounts) {
   const idleMintr = "0x074306BC6a6Fc1bD02B425dd41D742ADf36Ca9C6";
+  const gaugeController = "0xaC69078141f76A1e257Ee889920d02Cc547d632f";
   const contractList = getContract();
   const admin = accounts[0];
   const poolManager = await PoolManager.at(contractList.system.poolManager);
   const boosterAdd = contractList.system.idle_booster;
+  const booster = await Booster.at(contractList.system.idle_booster);
 
   const idle = await IERC20.at(contractList.system.idle_address);
   const stashToken = await IERC20.at(contractList.system.idle_stashtoken);
@@ -41,55 +48,34 @@ module.exports = async function (deployer, network, accounts) {
     "0xe4e69ef860d3018b61a25134d60678be8628f780",
   ];
 
-  logTransaction(
-    await poolManager.addPool("0x675eC042325535F6e176638Dd2d4994F645502B9", boosterAdd, 3, 3),
-    "add gauge AATranche_lido"
-  );
-
   await fundLpToken(lp_tokens, lp_tokens_users, admin);
+
+  const gaugeControllerContract = new web3.eth.Contract(gaugeControllerABI, gaugeController);
+  const gaguesCount = toBN(await gaugeControllerContract.methods.n_gauges().call()).toNumber();
+  console.log("idle gagues count " + gaguesCount);
+
+  for (var i = 0; i < gaguesCount; i++) {
+    // a workaround to add second pool in the first position for testing purpose
+    if (i == 0) continue;
+    let gauge = (await gaugeControllerContract.methods.gauges(i).call()).toString();
+
+    const gauge_type = (await gaugeControllerContract.methods.gauge_types(gauge).call()).toString();
+    if (gauge_type != "0") continue;
+
+    logTransaction(await poolManager.addPool(gauge, boosterAdd, 3, 3), "add gauge " + gauge);
+    if (i == gaguesCount - 1) {
+      gauge = (await gaugeControllerContract.methods.gauges(0).call()).toString();
+      logTransaction(await poolManager.addPool(gauge, boosterAdd, 3, 3), "add gauge " + gauge);
+    }
+  }
 
   // fund minter contract with veasset
   logTransaction(await idle.transfer(idleMintr, web3.utils.toWei("1000"), { from: admin }), "fund idle to minter");
   // fund gauge with stash token
   logTransaction(
-    await stashToken.transfer("0x675eC042325535F6e176638Dd2d4994F645502B9", web3.utils.toWei("1000"), {
+    await stashToken.transfer((await booster.poolInfo(0)).gauge, web3.utils.toWei("1000"), {
       from: "0x09f82ccd6bae2aebe46ba7dd2cf08d87355ac430",
     }),
     "fund stash token to gauge"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x21dDA17dFF89eF635964cd3910d167d562112f57", boosterAdd, 3, 3),
-    "add gauge AATranche_crvALUSD"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x7ca919Cf060D95B3A51178d9B1BCb1F324c8b693", boosterAdd, 3, 3),
-    "add gauge AATranche_frax"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x8cC001dd6C9f8370dB99c1e098e13215377Ecb95", boosterAdd, 3, 3),
-    "add gauge AATranche_mim"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0xDfB27F2fd160166dbeb57AEB022B9EB85EA4611C", boosterAdd, 3, 3),
-    "add gauge AATranche_3eur"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x30a047d720f735Ad27ad384Ec77C36A4084dF63E", boosterAdd, 3, 3),
-    "add gauge AATranche_stecrv"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0xAbd5e3888ffB552946Fc61cF4C816A73feAee42E", boosterAdd, 3, 3),
-    "add gauge AATranche_musd"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x41653c7AF834F895Db778B1A31EF4F68Be48c37c", boosterAdd, 3, 3),
-    "add gauge AATranche_mstable"
   );
 };
--- migrations/25_deploy_angle_pools.js
+++ migrations/25_deploy_angle_pools.js
@@ -1,8 +1,13 @@
 const { getContract } = require("./helper/addContracts");
 const { logTransaction } = require("./helper/logger.js");
-
+const Booster = artifacts.require("Booster");
 const PoolManager = artifacts.require("PoolManager");
 const IERC20 = artifacts.require("IERC20");
+const BigNumber = require("bignumber.js");
+const gaugeControllerABI = require("./helper/gaugeControllerABI.json");
+function toBN(number) {
+  return new BigNumber(number);
+}
 
 async function fundLpToken(lp_tokens, lp_tokens_users, to) {
   for (var i = 0; i < lp_tokens_users.length; i++) {
@@ -18,70 +23,45 @@ async function fundLpToken(lp_tokens, lp_tokens_users, to) {
 
 module.exports = async function (deployer, network, accounts) {
   const contractList = getContract();
+  const gaugeController = "0x9aD7e7b0877582E14c17702EecF49018DD6f2367";
   const admin = accounts[0];
   const poolManager = await PoolManager.at(contractList.system.poolManager);
   const boosterAdd = contractList.system.angle_booster;
+  const booster = await Booster.at(contractList.system.angle_booster);
   const angle = await IERC20.at(contractList.system.angle_address);
 
   const lp_tokens = [
-    "0x7B8E89b0cE7BAC2cfEC92A371Da899eA8CBdb450",
     "0x9C215206Da4bf108aE5aEEf9dA7caD3352A36Dad",
+    "0x7B8E89b0cE7BAC2cfEC92A371Da899eA8CBdb450",
     "0x5d8D3Ac6D21C016f9C935030480B7057B21EC804",
     "0xb3B209Bb213A5Da5B947C56f2C770b3E1015f1FE",
     "0xEDECB43233549c51CC3268b5dE840239787AD56c",
   ];
   const lp_tokens_users = [
-    "0x5aB0e4E355b08e692933c1F6f85fd0bE56aD18A6",
     "0xea51ccb352aea7641ff4d88536f0f06fd052ef8f",
+    "0x5aB0e4E355b08e692933c1F6f85fd0bE56aD18A6",
     "0xa116f421ff82a9704428259fd8cc63347127b777",
     "0xa2dee32662f6243da539bf6a8613f9a9e39843d3",
     "0x5be876ed0a9655133226be302ca6f5503e3da569",
   ];
-
-  logTransaction(
-    await poolManager.addPool("0x8E2c0CbDa6bA7B65dbcA333798A3949B07638026", boosterAdd, 3, 4),
-    "add gauge sanDAI_EUR"
-  );
-
   await fundLpToken(lp_tokens, lp_tokens_users, admin);
+
+  const gaugeControllerContract = new web3.eth.Contract(gaugeControllerABI, gaugeController);
+  const gaguesCount = toBN(await gaugeControllerContract.methods.n_gauges().call()).toNumber();
+  console.log("angle gagues count " + gaguesCount);
+
+  for (var i = 0; i < gaguesCount; i++) {
+    let gauge = (await gaugeControllerContract.methods.gauges(i).call()).toString();
+
+    const gauge_type = (await gaugeControllerContract.methods.gauge_types(gauge).call()).toString();
+    if (gauge_type != "0") continue;
+
+    logTransaction(await poolManager.addPool(gauge, boosterAdd, 3, 4), "add gauge " + gauge);
+  }
+
   // fund gauge with veasset
   logTransaction(
-    await angle.transfer("0x8E2c0CbDa6bA7B65dbcA333798A3949B07638026", web3.utils.toWei("1000"), { from: admin }),
+    await angle.transfer((await booster.poolInfo(0)).gauge, web3.utils.toWei("1000"), { from: admin }),
     "fund angle to gauge"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x51fE22abAF4a26631b2913E417c0560D547797a7", boosterAdd, 3, 4),
-    "add gauge sanUSDC_EUR"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x7c0fF11bfbFA3cC2134Ce62034329a4505408924", boosterAdd, 3, 4),
-    "add gauge sanFEI_EUR"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0xb40432243E4F317cE287398e72Ab8f0312fc2FE8", boosterAdd, 3, 4),
-    "add gauge sanFRAX_EUR"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0xEB7547a8a734b6fdDBB8Ce0C314a9E6485100a3C", boosterAdd, 3, 4),
-    "add gauge Gelato Uni-V3 agEUR/USDC"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0x3785Ce82be62a342052b9E5431e9D3a839cfB581", boosterAdd, 3, 4),
-    "add gauge Gelato Uni-V3 agEUR/wETH"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0xBa625B318483516F7483DD2c4706aC92d44dBB2B", boosterAdd, 3, 4),
-    "add gauge SushiSwap agEUR/ANGLE"
-  );
-
-  logTransaction(
-    await poolManager.addPool("0xd6282C5aEAaD4d776B932451C44b8EB453E44244", boosterAdd, 3, 4),
-    "add gauge Uni-V2 agEUR/FEI"
   );
 };
